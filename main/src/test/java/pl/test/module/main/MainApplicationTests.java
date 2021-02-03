2a778f5acfbdd470cb8e70148d9cec897853698b
treda <tomekreda16@gmail.com>
2021-02-03 21:08:26 +0100

    Dodanie aplikkacji

---
+++ main/src/test/java/pl/test/module/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.test.module.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
