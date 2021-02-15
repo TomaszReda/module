b259b359641c98d54cd4b9c7a58deb0870f23083
treda <tomekreda16@gmail.com>
2021-02-15 22:00:00 +0100

    MAin app

---
+++ mainapp/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+open module mainapp {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+}
--- mainapp/src/main/java/pl/test/mainapp/MainappApplication.java
+++ mainapp/src/main/java/pl/test/mainapp/MainappApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainappApplication {
 
     public static void main(String[] args) {
+
+        System.err.println("aaaa");
         SpringApplication.run(MainappApplication.class, args);
     }
 
